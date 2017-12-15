4941a831d8c763bbc8130e209f0ed30bb9693681
Ser-DSTChile\Ser - DST Chile <luis.espinoza@dstchile.cl>
2017-12-15 12:55:14 -0300

    conexion

--- platforms/browser/www/js/conexion.js
+++ platforms/browser/www/js/conexion.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 
-var servidor='http://192.168.1.41/';
-var servidor1='http://192.168.1.41/bodega_lodis';
-//var servidor='https://www.sistemalodis.cl';
-//var servidor1='https://www.sistemalodis.cl';
+//var servidor='http://192.168.1.41/';
+//var servidor1='http://192.168.1.41/bodega_lodis';
+var servidor='https://www.sistemalodis.cl';
+var servidor1='https://www.sistemalodis.cl';
