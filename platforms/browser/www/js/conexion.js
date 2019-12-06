df1ab64f67e113e41c401f265de5214a9e7d9997
Ser-DSTChile\Ser - DST Chile <luis.espinoza@dstchile.cl>
2019-12-06 16:44:38 -0300

    firma y rut

--- platforms/browser/www/js/conexion.js
+++ platforms/browser/www/js/conexion.js
@@ -2,10 +2,10 @@
 
 //var servidor='http://servidordstchile2.ddns.net/';
 //var servidor1='http://servidordstchile2.ddns.net/bodega_lodis';
-var servidor='http://192.168.1.41/';
-var servidor1='http://192.168.1.41/bodega_lodis';
+//var servidor='http://192.168.1.41/';
+//var servidor1='http://192.168.1.41/bodega_lodis';
 
 
 
-//var servidor='https://www.sistemalodis.cl';
-//var servidor1='https://www.sistemalodis.cl';
+var servidor='https://www.sistemalodis.cl';
+var servidor1='https://www.sistemalodis.cl';
