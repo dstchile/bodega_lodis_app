882355b48fe2316f70c571433d7ae1f5dd518538
Ser-DSTChile\Ser - DST Chile <luis.espinoza@dstchile.cl>
2021-05-06 19:31:46 -0300

    prueba de plugin camera

--- platforms/browser/www/js/conexion.js
+++ platforms/browser/www/js/conexion.js
@@ -2,12 +2,14 @@
 
 //var servidor='https://servidordstchile.ddns.net/';
 //var servidor1='https://servidordstchile.ddns.net/bodega_lodis';
+//var servidor='https://servidordstchile.ddns.net/';
+//var servidor1='https://servidordstchile.ddns.net/carozzi.lodis.cl';
 //var servidor='http://192.168.1.41/';
 //var servidor1='http://192.168.1.41/bodega_lodis';
 
 
 
-//var servidor='https://www.sistemalodis.cl';
-//var servidor1='https://www.sistemalodis.cl';
-var servidor='https://carozzi.lodis.cl';
-var servidor1='https://carozzi.lodis.cl';
+var servidor='https://www.sistemalodis.cl';
+var servidor1='https://www.sistemalodis.cl';
+//var servidor='https://carozzi.lodis.cl';
+//var servidor1='https://carozzi.lodis.cl';
